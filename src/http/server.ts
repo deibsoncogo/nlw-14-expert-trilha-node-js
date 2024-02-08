c39abdca9e64a41c621d27365be52724c038d07e
Deibson Cogo <deibsoncogo@outlook.com>
2024-02-07 21:34:27 -0300

    💄 style: implementação do eslint

--- src/http/server.ts
+++ src/http/server.ts
@@ -8,13 +8,13 @@ const prisma = new PrismaClient()
 
 app.post("/polls", async (request, reply) => {
   const createPollBody = z.object({
-    title: z.string()
+    title: z.string(),
   })
 
   const { title } = createPollBody.parse(request.body)
 
   const poll = await prisma.poll.create({
-    data: { title }
+    data: { title },
   })
 
   return reply.status(201).send({ pollId: poll.id })
